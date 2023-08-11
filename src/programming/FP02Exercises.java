314632816a57209c0e6b240de126748a66081a10
alexnahayo <alex.nahayo@globalpay.com>
2023-08-11 14:41:06 +0100

    adding to FP02Exercises class along with completed exercises

--- src/programming/FP02Exercises.java
+++ src/programming/FP02Exercises.java
@@ -1,13 +1,16 @@
 package programming;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FP02Exercises {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(12,9,13,4,6,2,4,12,15);
-        int sum =  squareNumberInListAndSum(numbers);
-        System.out.println(sum);
+        List<String> courses = Arrays.asList( "Spring", "Spring Boot", "API" , "Microservices", "AWS", "PCF","Azure", "Docker", "Kubernetes");
+       // lengthOfCourseTitleList(courses);
+        System.out.println(lengthOfCourseTitleList(courses));
     }
 
     private static int  squareNumberInListAndSum(List<Integer> numbers){
@@ -16,5 +19,50 @@ public class FP02Exercises {
                 .reduce(0, Integer::sum);
     }
 
+    private static int  cubeNumberInListAndSum(List<Integer> numbers){
+        return numbers.stream()
+                .map(number-> number*number*number)
+                .reduce(0, Integer::sum);
+    }
+
+    private static int  oddNumberInListAndSum(List<Integer> numbers){
+        return numbers.stream()
+                .filter(number-> number % 2 == 1 )
+                .reduce(0, Integer::sum);
+    }
+
+    private static int  evenNumberInListAndSum(List<Integer> numbers){
+        return numbers.stream()
+                .filter(number-> number % 2 == 0 )
+                .reduce(0, Integer::sum);
+    }
+
+    private static void  distinctNumberInAList(List<Integer> numbers){
+      numbers.stream().distinct().forEach(System.out::println);
+    }
+
+    private static void  sortNumberInAList(List<Integer> numbers){
+        numbers.stream().sorted().forEach(System.out::println);
+    }
+
+    private static void  sortAndDistictNumberInAList(List<Integer> numbers){
+        numbers.stream().distinct().sorted().forEach(System.out::println);
+    }
+
+    private static void  reverseListInAscendingOrder(List<Integer> numbers){
+        numbers.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+    }
+
+    public static List<Integer> evenNumberFilteredList(List<Integer> numbers){
+        return numbers.stream()
+                .filter(number ->  number % 2 == 0)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Integer> lengthOfCourseTitleList(List<String> courses){
+        return courses.stream()
+                .map(String::length)
+                .collect(Collectors.toList());
+    }
 
 }
--- src/programming/FP02Functional.java
+++ src/programming/FP02Functional.java
@@ -2,12 +2,13 @@ package programming;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FP02Functional {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(12,9,13,4,6,2,4,12,15);
-        int sum = addListFunctional(numbers);
-        System.out.println(sum);
+        List<Integer> squareNumbers = squareList(numbers);
+        System.out.println(squareNumbers);
     }
 
     private static int sum (int a, int b){
@@ -19,6 +20,11 @@ public class FP02Functional {
                 .reduce(0, FP02Functional::sum);
                 ///.reduce(0, (x,y) -> x+y);
                 ///.reduce(0, Integer::sum);
+    }
+    private static List<Integer> squareList(List<Integer> numbers) {
+        return numbers.stream()
+                .map(number -> number * number)
+                .collect(Collectors.toList());
     }
 
 
